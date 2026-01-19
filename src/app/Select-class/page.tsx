cd0f48f81857e4495f242365a379fbef31513b26
Dmytro <dmitr.dubas@gmail.com>
2026-01-19 13:31:44 +0200

    Character selection + background

---
+++ src/app/Select-class/page.tsx
@@ -0,0 +1,9 @@
+import ClassSelection from '../../components/ClassSelection/ClassSelection';
+
+export default function ClassSelectionPage() {
+    return (
+        <main>
+            <ClassSelection />
+        </main>
+    );
+}
